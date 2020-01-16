3b9cba69892cfef7714cbc82d6839d7d1ee6291a
Michael Chen <michaelchengaming@gmail.com>
2020-01-15 22:22:26 -0800

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,18 @@
 // Code DelayedButton Component Here
+import React, { Component } from "react";
+
+export default class DelayedButton extends Component {
+  handleClick = event => {
+    event.persist();
+    let ms = this.props.delay;
+    setTimeout(() => this.props.onDelayedClick(event), ms);
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>VROOM!</button>
+      </div>
+    );
+  }
+}
